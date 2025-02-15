522da9a2c07990651e1586cd0b6ab4f834220f51
Kyoge0 <kyogesanada@gmail.com>
2025-02-14 20:01:23 -0600

    Facebook instagam?

--- Assets/JS/login.js
+++ Assets/JS/login.js
@@ -3,32 +3,7 @@ $(document).ready(function () {
     event.preventDefault();
     var username = $("#username").val().trim();
     var password = $("#password").val().trim();
-    if (username === "" || password === "") {
-      alert("Por favor, completa todos los campos.");
-    } else if (username === "demoUser" && password === "demoPass") {
-      alert("Login exitoso. Redirigiendo al timeline...");
-      window.location.href = "timeline.html";
-    } else {
-      alert("Usuario o contraseña incorrectos.");
-    }
-  });
-});
-
-$(document).ready(function () {
-  // Genera un número aleatorio para obtener una imagen diferente cada vez
-  var randomNumber = Math.floor(Math.random() * 1000);
-  var imageUrl = "https://picsum.photos/1920/1080?random=" + randomNumber;
-
-  // Asigna la imagen al background del body
-  $("body").css("background-image", "url(" + imageUrl + ")");
-});
-
-$(document).ready(function () {
-  $("#loginForm").on("submit", function (event) {
-    event.preventDefault();
-    var username = $("#username").val().trim();
-    var password = $("#password").val().trim();
-
+    
     if (username === "" || password === "") {
       alert("Por favor, completa todos los campos.");
     } else if (username === "demoUser" && password === "demoPass") {
@@ -47,4 +22,9 @@ $(document).ready(function () {
       alert("Usuario o contraseña incorrectos.");
     }
   });
+
+  // Genera un número aleatorio para obtener una imagen diferente cada vez
+  var randomNumber = Math.floor(Math.random() * 1000);
+  var imageUrl = "https://picsum.photos/1920/1080?random=" + randomNumber;
+  $("body").css("background-image", "url(" + imageUrl + ")");
 });
